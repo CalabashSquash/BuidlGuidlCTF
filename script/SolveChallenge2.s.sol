c795d4e647ed58e68f5213450f672a94a2ff8ad5
CalabashSquash <my_name_uhh@protonmail.com>
2024-11-28 21:08:29 +1100

    Challenge 2

---
+++ script/SolveChallenge2.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Challenge2} from "../src/Challenge2/Challenge2.sol";
+import {Solution2} from "../src/Challenge2/Solution2.sol";
+import {NFT} from "src/NFT/NFT.sol";
+
+contract SolveChallenge2 is Script {
+    Challenge2 public challenge2;
+    NFT public nft;
+
+    function setUp() public {
+        nft = new NFT();
+        challenge2 = new Challenge2(address(nft));
+    }
+
+    function run() public {
+        vm.startBroadcast();
+        setUp();
+
+        Solution2 solution2 = new Solution2(address(challenge2), address(nft));
+
+        require(nft.ownerOf(2) == msg.sender, "Didn't get NFT");
+
+        vm.stopBroadcast();
+    }
+}
---
+++ src/Challenge2/Challenge2.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+interface INFTFlags {
+    function mint(address _recipient, uint256 _challengeId) external;
+}
+
+contract Challenge2 {
+    address public nftContract;
+    constructor(address _nftContract) {
+        nftContract = _nftContract;
+    }
+
+    function justCallMe() public {
+        require(msg.sender != tx.origin, "Not allowed");
+        INFTFlags(nftContract).mint(tx.origin, 2);
+    }
+}
